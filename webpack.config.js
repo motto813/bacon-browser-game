999834bed9a8d86139c43b192ba7c728b4275bac
motto813 <hammerschmidtpaul@gmail.com>
2017-11-03 03:56:45 -0500

    cheap eval source map used if production for webpack optimization FOR REAL THIS TIME

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
   context: __dirname,
   entry: "./js/ClientApp.jsx",
+  devtool: process.env.NODE_ENV === "development" ? "cheap-eval-source-map" : false,
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js",
